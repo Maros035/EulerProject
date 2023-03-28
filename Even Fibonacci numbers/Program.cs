77ea55dc489aa98aefd0fb71734298d73314094f
Maroš Siber <maros035035@gmail.com>
2023-03-28 16:37:46 +0200

    Even Fibonacci numbers

---
+++ Even Fibonacci numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Even_Fibonacci_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong a = 0;
+            ulong b = 1;
+            ulong c = 0;
+            ulong sum = 0;
+
+            //Fibonacci
+            for (ulong i = 0; c < 4000000; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+
+            //even num
+                if (c % 2 == 0)
+                {
+                    sum += c;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
--- Multiples of 3 or 5/Program.cs
+++ Multiples of 3 or 5/Program.cs
@@ -14,7 +14,6 @@
                 }
             }
             Console.WriteLine(sum);
-            //komentar
         }
     }
 }
