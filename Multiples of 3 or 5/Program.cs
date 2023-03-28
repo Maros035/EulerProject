b88499f77d77f3a7ac577802177dfc734744636e
Maroš Siber <maros035035@gmail.com>
2023-03-28 13:55:47 +0200

    Solution - Multiples of 3 or 5

---
+++ Multiples of 3 or 5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Multiples_of_3_or_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                //delitelne 3
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
