22b628f7ac0c748973302e514cbe7fa3a043eae7
Maroš Siber <maros035035@gmail.com>
2023-04-02 11:39:56 +0200

    Solved - Euler_9_Special Pythagorean triplet

---
+++ 9_Special Pythagorean triplet/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _9_Special_Pythagorean_triplet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 1, c = 0, abcSum = 1000;
+
+            for (a = 1; a < abcSum; a++) 
+            {
+                for (b = a + 1; b < abcSum; b++)
+                {
+                    c = abcSum - (a + b);
+
+                    if (a * a + b * b == c * c)
+                    {
+                        Console.WriteLine(a * b * c);
+                    }
+                }
+            }
+        }
+    }
+}
